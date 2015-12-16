10b9756cf108cfb65f67722d914e7d969ca96eef
chachora <chachora@gmail.com>
2015-12-16 12:28:59 -0800

    Add mustache templates, access to lirc remotes DB

--- app.js
+++ app.js
@@ -3,78 +3,30 @@
  * 
  * IR Remote Web Service
  * 
- * @author Michael Vartan
- * @version 1.0.0 
+  * @version 0.1.0
  */
 
 var express = require('express');
 var app = express();
 var sys = require('util')
 var exec = require('child_process').exec;
+var lircRemotes = require('./lib/lirc-remotes');
+var mustache = require('mustache-express');
 
-/**
- * Dictionary of devices and their buttons
- * @type {Object}
- */
-var devices = {};
-/**
- * Generates function to get devices' buttons from irsend command
- * @param  {String} deviceName name of device
- * @return {Function}            exec callback
- */ 
-var getCommandsForDevice = function(deviceName) {
-  /**
-   * Get Device's Button from irsend command
-   * @param  {String} error  Error from running command
-   * @param  {String} stdout std out
-   * @param  {String} stderr std err
-   * @return {None}        
-   */
-  return function(error, stdout, stderr) {
-    var lines = stderr.split("\n");
-    for(var lineIndex in lines) {
-      var line = lines[lineIndex];
-      var parts = line.split(" ");
-      if(parts.length>2) {
-        var keyName = parts[2];
-        devices[deviceName].push(keyName);
-        console.log(deviceName + " found key: "+keyName);
-      }
-    }
-  }
-};
-/**
- * Get Device from irsend command
- * @param  {String} error  Error from running command
- * @param  {String} stdout std out
- * @param  {String} stderr std err
- * @return {None}        
- */
-var getDevice = function (error, stdout, stderr) {
-  if(error) {
-    console.log("irsend not available.");
-    return;
-  }
-  var lines = stderr.split("\n");
-  for(var lineIndex in lines) {
-    var line = lines[lineIndex];
-    var parts = line.split(" ");
-    if(parts.length>1) {
-      var deviceName = parts[1];
-      console.log("device found: "+deviceName.trim());
-      devices[deviceName] = [];
-      exec("irsend list \""+deviceName+"\" \"\"", getCommandsForDevice(deviceName));
-
-    }
-  }          
-};
-// Get all device information
-exec("irsend list \"\" \"\"", getDevice);
-
-// Define static HTML files
-app.use(express.static(__dirname + '/html'));
+// Define templates engine
+app.engine('html', mustache());
+app.set('view engine', 'html');
+app.set('views', __dirname +'/views')
+app.use(express.static(__dirname + '/assets'));
 
 
+app.get('/devices/add', function(req,res){
+    lircRemotes.getAllBrands(function(brands){
+        res.render('devices_add', {
+            brands: brands
+        })
+    });
+});
 
 // define GET request for /send/deviceName/buttonName
 app.get('/send/:device/:key', function(req, res) {
