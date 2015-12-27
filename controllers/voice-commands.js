d43bb4a57ade2047e4cf1828016e00e230cf2ad2
chachora <chachora@gmail.com>
2015-12-27 23:21:39 +0400

    Add voice commands list html template

--- controllers/voice-commands.js
+++ controllers/voice-commands.js
@@ -2,10 +2,14 @@
  * Created by chachora on 12/23/15.
  */
 
+var lircDevices = require("../lib/lirc-devices");
 var voiceCommandsConf = require("../lib/voice-commands-conf.js");
+
 
 exports.view = function(req,res){
         res.render("voice_commands.html", {
-                commands: voiceCommandsConf.getVoiceCommands()
+                commands: voiceCommandsConf.getVoiceCommands(),
+                buttons: lircDevices.getButtons(),
+                devices: lircDevices.getDevices()
         });
 };
